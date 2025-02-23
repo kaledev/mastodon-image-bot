c57a8e81e1d0a6077f96d44adb38409f1ff7b5ac
Mike <kaledev@gmail.com>
2025-02-22 19:49:27 -0500

    typo

--- bird/bird.py
+++ bird/bird.py
@@ -17,7 +17,7 @@ from datetime import datetime, timedelta
 ERROR_FILE = "birdbot_error_time.txt"
 
 # Mastodon API credentials
-MASTODON_BASE_URL = os.getenv('"MASTODON_BASE_URL')
+MASTODON_BASE_URL = os.getenv('MASTODON_BASE_URL')
 MASTODON_ACCESS_TOKEN = os.getenv('MASTODON_ACCESS_TOKEN')
 
 # OpenAI API credentials
